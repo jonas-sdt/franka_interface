b98b7577b755044f235449b61f68867e631d810c
Jonas Schmidt <jonas.schmidt@schanzer-racing.de>
2023-06-30 14:29:58 +0200

    split up definition and implementation for utils

---
+++ franka_interface/src/franka_interface/utils.cpp
@@ -0,0 +1,124 @@
+#include "franka_interface/utils.hpp"
+
+namespace franka_interface
+{
+    geometry_msgs::PoseStamped make_pose_stamped(float x, float y, float z, float qx, float qy, float qz, float qw, std::string frame_id)
+    {
+        geometry_msgs::PoseStamped pose_stamped;
+        pose_stamped.pose.position.x = x;
+        pose_stamped.pose.position.y = y;
+        pose_stamped.pose.position.z = z;
+        pose_stamped.pose.orientation.x = qx;
+        pose_stamped.pose.orientation.y = qy;
+        pose_stamped.pose.orientation.z = qz;
+        pose_stamped.pose.orientation.w = qw;
+        pose_stamped.header.frame_id = frame_id;
+        return pose_stamped;
+    }
+
+    geometry_msgs::PoseStamped make_pose_stamped(float x, float y, float z, float roll, float pitch, float yaw, std::string frame_id)
+    {
+        tf2::Quaternion q;
+        q.setRPY(roll, pitch, yaw);
+        return make_pose_stamped(x, y, z, q.x(), q.y(), q.z(), q.w(), frame_id);
+    }
+
+    geometry_msgs::Pose make_pose(float x, float y, float z, float qx, float qy, float qz, float qw)
+    {
+        geometry_msgs::Pose pose;
+        pose.position.x = x;
+        pose.position.y = y;
+        pose.position.z = z;
+        pose.orientation.x = qx;
+        pose.orientation.y = qy;
+        pose.orientation.z = qz;
+        pose.orientation.w = qw;
+        return pose;
+    }
+
+    geometry_msgs::Pose make_pose(float x, float y, float z, float roll, float pitch, float yaw)
+    {
+        tf2::Quaternion q;
+        q.setRPY(roll, pitch, yaw);
+        return make_pose(x, y, z, q.x(), q.y(), q.z(), q.w());
+    }
+
+    std::string pose_stamped_to_string(const geometry_msgs::PoseStamped & pose_stamped)
+    {
+        std::string str = "PoseStamped: ";
+        str += "frame_id: " + pose_stamped.header.frame_id + ", \n";
+        str += "position: (" + std::to_string(pose_stamped.pose.position.x) + ", " + std::to_string(pose_stamped.pose.position.y) + ", " + std::to_string(pose_stamped.pose.position.z) + "), \n";
+        str += "orientation: (" + std::to_string(pose_stamped.pose.orientation.x) + ", " + std::to_string(pose_stamped.pose.orientation.y) + ", " + std::to_string(pose_stamped.pose.orientation.z) + ", " + std::to_string(pose_stamped.pose.orientation.w) + ")";
+        return str;
+    }
+
+    std::string pose_to_string(const geometry_msgs::Pose & pose)
+    {
+        std::string str = "Pose: ";
+        str += "position: (" + std::to_string(pose.position.x) + ", " + std::to_string(pose.position.y) + ", " + std::to_string(pose.position.z) + "), \n";
+        str += "orientation: (" + std::to_string(pose.orientation.x) + ", " + std::to_string(pose.orientation.y) + ", " + std::to_string(pose.orientation.z) + ", " + std::to_string(pose.orientation.w) + ")";
+        return str;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const geometry_msgs::PoseStamped& pose_stamped)
+    {
+        os << pose_stamped_to_string(pose_stamped);
+        return os;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const geometry_msgs::Pose& pose)
+    {
+        os << pose_to_string(pose);
+        return os;
+    }
+
+    long double operator"" _m(unsigned long long int value)
+    {
+        return value;
+    }
+
+    long double operator"" _m(long double value)
+    {
+        return value;
+    }
+
+    long double operator"" _cm(unsigned long long int value)
+    {
+        return value/100.0;
+    }
+
+    long double operator"" _cm(long double value)
+    {
+        return value/100.0;
+    }
+
+    long double operator"" _mm(unsigned long long int value)
+    {
+        return value/1000.0;
+    }
+
+    long double operator"" _mm(long double value)
+    {
+        return value/1000.0;
+    }
+
+    long double operator"" _deg(unsigned long long int value)
+    {
+        return value*M_PI/180.0;
+    }
+
+    long double operator"" _deg(long double value)
+    {
+        return value*M_PI/180.0;
+    }
+
+    long double operator"" _rad(unsigned long long int value)
+    {
+        return value;
+    }
+
+    long double operator"" _rad(long double value)
+    {
+        return value;
+    }
+} // namespace franka_interface
